f76d8b26286443c6b868cb9aa7370c76aa1feb06
Viktor Filippov <jackal3166@gmail.com>
2024-02-20 01:27:43 +0200

    Create App

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+	<main
+		class="flex items-center justify-center min-h-screen p-10 bg-slate-900 text-slate-400"
+	>
+		<h1 class="text-3xl font-bold underline">Hello world!</h1>
+	</main>
+</template>
